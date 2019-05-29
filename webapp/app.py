f9ec7e4b6436ffed9c04b705425d618886578ace
Kerem <keremgocen@gmail.com>
2019-05-29 16:45:44 +0300

    add arbitrary update and create endpoints

---
+++ webapp/app.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+from flask import Flask, jsonify, request
+
+from database import db_session, init_db
+from models import User
+
+app = Flask(__name__)
+init_db()
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
+@app.route("/users")
+def hello():
+    return jsonify([x.json_rep() for x in User.query.all()])
+
+
+@app.route("/create", methods=['POST'])
+def add_user():
+    req_data = request.get_json()
+    if req_data is None:
+        return jsonify("empty request body"), 400
+
+    email = req_data.get('email')
+    if email is None:
+        return jsonify("email is required in request body"), 400
+
+    if db_session.query(User.id).filter_by(email=email).scalar() is not None:
+        return jsonify("user already created"), 400
+
+    u = User(email)
+    u.first_name = req_data.get('first_name')
+    u.last_name = req_data.get('last_name')
+    u.created_at = datetime.now()
+    u.updated_at = datetime.now()
+
+    db_session.add(u)
+    db_session.commit()
+
+    return jsonify(u.json_rep())
+
+
+@app.route("/update",  methods=['POST'])
+def update_user():
+    req_data = request.get_json()
+    if req_data is None:
+        return jsonify("req_data problem"), 400
+
+    email = req_data['email']
+    u = User.query.filter(User.email == email).first()
+    if u is None:
+        return jsonify("user not found"), 400
+
+    if req_data.items() is None:
+        return jsonify("empty request"), 400
+
+    u.assign_properties(**req_data)
+    u.updated_at = datetime.now()
+
+    db_session.commit()
+
+    return jsonify(u.json_rep())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
---
+++ webapp/models.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from flask import jsonify
+
+from database import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(120), unique=True, nullable=False)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    created_at = Column(DateTime, nullable=False)
+    updated_at = Column(DateTime, nullable=False)
+    deleted_at = Column(DateTime, index=True)
+
+    def __init__(self, email=None):
+        self.email = email
+
+    def assign_properties(self, **kwargs):
+        print(','.join('{0}={1!r}'.format(k, v) for k, v in kwargs.items()))
+        for prop, val in kwargs.items():
+            if hasattr(self, prop):
+                print("updating property {0}={1}".format(prop, val))
+                setattr(self, str(prop), val)
+
+    def json_rep(self):
+        data = {
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at,
+            'deleted_at': self.deleted_at
+        }
+        return data
+
+    def __repr__(self):
+        return "<User {}>".format(self.first_name)
